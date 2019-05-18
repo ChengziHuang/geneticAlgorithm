4a8b1a35b38051391f30b3b9389ea86d7cbd759f
czh <huangchengzi0711@outlook.com>
2019-05-18 21:08:37 +0800

    version one

--- code/simpleGA/isFinish.m
+++
@@ -1,9 +0,0 @@
-function result = isFinish(individual)
-    result = 1;
-    for gene = 1 : size(individual, 2)
-        if individual(gene) == 0
-            result = 0;
-            break;
-        end
-    end
-end
